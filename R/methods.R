5d004ac0e9232a1b2b15f262c3c2190bb1b0478f
sofiejorgensen <sofficial.jorgensen@gmail.com>
2020-09-18 13:48:04 +0200

    added aes_string to importFrom

--- R/methods.R
+++ R/methods.R
@@ -31,7 +31,7 @@ print.linreg <- function(x, ...) {
 #' @param ... 
 #'
 #' @return
-#' @importFrom ggplot2 ggplot aes geom_point stat_summary geom_smooth geom_text geom_hline ggtitle
+#' @importFrom ggplot2 ggplot aes geom_point aes_string stat_summary geom_smooth geom_text geom_hline ggtitle
 #' @export
 #'
 #' @examples
